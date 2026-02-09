81760312fcf688149830364300c8a54929e3a75d
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-02-09 16:07:28 +0100

    Extend Customer API with `Customer` model, new REST endpoints, and integration test for resource creation and retrieval

--- customer-api-provider/src/main/java/de/samples/quarkus/CustomersResource.java
+++ customer-api-provider/src/main/java/de/samples/quarkus/CustomersResource.java
@@ -1,17 +1,42 @@
 package de.samples.quarkus;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @Path("/customers")
 public class CustomersResource {
+
+  private final Map<UUID, Customer> customers = new HashMap<>();
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   public String getAllCustomers() {
     return "[]";
+  }
+
+  // TODO prüfen, ob notwendig?
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Customer createCustomer(Customer customer) {
+    // ???
+    return customer;
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/{uuid}")
+  public Customer findCustomerById(@PathParam("uuid") UUID uuid) {
+    return customers.get(uuid); // TODO ???
   }
 
 }
--- customer-api-provider/src/test/java/de/samples/quarkus/CustomerApiTests.java
+++ customer-api-provider/src/test/java/de/samples/quarkus/CustomerApiTests.java
@@ -7,7 +7,11 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 @QuarkusTest
 class CustomerApiTests {
@@ -28,5 +32,57 @@ class CustomerApiTests {
   // GET /customers + Accept: XML -> 406
 
   // state-Parameter? -> active/locked/disabled / gelbekatze
+
+
+  @Test
+  void whenPostCustomers_Then_ReturnLocationHeaderToFetchNewResource() {
+
+    // --- POST ---
+    final var postResponse =
+      given()
+        .contentType(ContentType.JSON)
+        .accept(ContentType.JSON)
+        .body("""
+          {
+            "name": "Tom Mayer",
+            "birthdate": "2001-04-23",
+            "state": "active"
+          }
+          """)
+        .when()
+        .post("/customers")
+        .then()
+        .statusCode(201)
+        .contentType(ContentType.JSON)
+        .body("name", is(equalTo("Tom Mayer")))
+        .body("birthdate", is(equalTo("2001-04-23")))
+        .body("state", is(equalTo("active")))
+        .body("uuid", not(is(emptyOrNullString())))
+        .header("Location", not(is(emptyOrNullString())))
+        .extract()
+        .response();
+
+    // Location-Header auslesen
+    final var location = postResponse.getHeader("Location");
+    // UUID auslesen
+    final var uuidFromBody = postResponse.path("uuid");
+
+    // --- GET ---
+    given()
+      .accept(ContentType.JSON)
+      .when()
+      .get(location)
+      .then()
+      .statusCode(200)
+      .contentType(ContentType.JSON)
+      .body("uuid", is(equalTo(uuidFromBody)))
+      .body("name", is(equalTo("Tom Mayer")))
+      .body("birthdate", is(equalTo("2001-04-23")))
+      .body("state", is(equalTo("active")));
+  }
+
+  // Content Negotiation
+  // POST customers -> GET /customers/{uuid} -> 200
+  // POST customers -> GET /customers -> 200 + Kunde in Liste
 
 }
