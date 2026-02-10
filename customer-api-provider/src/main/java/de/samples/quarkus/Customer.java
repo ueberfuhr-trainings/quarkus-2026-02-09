c5ebd450bdc6afdd324fa75e3b69970d4126659e
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-02-10 09:23:33 +0100

    Integrate Lombok for code simplification and update project configuration

--- customer-api-provider/src/main/java/de/samples/quarkus/Customer.java
+++ customer-api-provider/src/main/java/de/samples/quarkus/Customer.java
@@ -1,52 +1,20 @@
 package de.samples.quarkus;
 
 import jakarta.json.bind.annotation.JsonbTransient;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
-// TODO Lombok
+@Getter
+@Setter
 public class Customer {
 
+  @Setter(onMethod_ = @JsonbTransient)
   private UUID uuid;
   private String name;
   private LocalDate birthdate;
   private String state;
 
-  public UUID getUuid() {
-    return uuid;
-  }
-
-  @JsonbTransient
-  public Customer setUuid(UUID uuid) {
-    this.uuid = uuid;
-    return this;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Customer setName(String name) {
-    this.name = name;
-    return this;
-  }
-
-  public LocalDate getBirthdate() {
-    return birthdate;
-  }
-
-  public Customer setBirthdate(LocalDate birthdate) {
-    this.birthdate = birthdate;
-    return this;
-  }
-
-  public String getState() {
-    return state;
-  }
-
-  public Customer setState(String state) {
-    this.state = state;
-    return this;
-  }
 }
