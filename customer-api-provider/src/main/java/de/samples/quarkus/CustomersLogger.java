c2e559d7ef74e76dcdf8e46b88a7a56a19a5a0fe
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2026-02-12 06:48:08 +0100

    Add configurable CustomersLogger with startup message and environment-specific properties (#13)

--- customer-api-provider/src/main/java/de/samples/quarkus/CustomersLogger.java
+++ customer-api-provider/src/main/java/de/samples/quarkus/CustomersLogger.java
@@ -1,11 +1,22 @@
 package de.samples.quarkus;
 
 import io.quarkus.arc.log.LoggerName;
+import io.quarkus.arc.properties.IfBuildProperty;
 import io.quarkus.runtime.Startup;
+import jakarta.inject.Inject;
 import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
+// Anforderung:
+// - Property application.customers-logger.enabled=true|false (default: false)
+// - Property application.customers-logger.startup.message=<string>
+
 // @Dependent
+@IfBuildProperty(
+  name = "application.customers-logger.enabled",
+  stringValue = "true"
+)
 @RequiredArgsConstructor
 public class CustomersLogger {
 
@@ -14,10 +25,17 @@ public class CustomersLogger {
   @LoggerName("customers")
   Logger log;
 
+  @Inject
+  @ConfigProperty(
+    name = "application.customers-logger.startup.message",
+    defaultValue = "Customers count: {0}"
+  )
+  String startupMessage;
+
   @Startup
   public void logCountOfCustomers() {
     var count = customersService.count();
-    log.infov("Customers count: {0}", count);
+    log.infov(this.startupMessage, count);
   }
 
 }
